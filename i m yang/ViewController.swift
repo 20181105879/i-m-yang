d17d5e2d9af23000223794a2e4df66c6d1904407
20181105879 <2605753145@qq.com>
2019-10-09 10:26:47 +0800

    Initial Commit

---
+++ i m yang/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i m yang
+//
+//  Created by s20181105879 on 2019/10/9.
+//  Copyright © 2019 s20181105879. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
